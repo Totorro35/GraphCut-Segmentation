2b38e0b58b489f039063669926ce7815fcffedcb
thomas <thomas.lemetayer.35@gmail.com>
2020-01-30 10:31:14 +0100

    up

--- script/main.py
+++ script/main.py
@@ -1,11 +1,70 @@
 import argparse
 import cv2
 import numpy as np
+import scipy
+from scipy.misc import imread
+import maxflow
+from matplotlib import pyplot as ppl
+import math
 
+
+## Global Var
 drawing=False # true if mouse is pressed
 mode=True
-
+alpha=0.4
 color=(0,0,255)
+
+def wij(x,y,sigma=1) :
+    factor= ((x-y)**2)/(2*sigma**2)
+    return math.exp(-factor)
+
+def delta_func(x,y) :
+    if x==y:
+        return 1
+    else :
+        return 0
+
+def R_func(img,value) :
+    return abs(value-img)
+
+def computeSegmentation(path,image):
+    F = {}
+    B = {}
+
+
+    img = imread(path,flatten=True)/255
+
+    # Create the graph.
+    g = maxflow.Graph[float]()
+    # Add the nodes. nodeids has the identifiers of the nodes in the grid.
+    nodeids = g.add_grid_nodes(img.shape)
+
+    # Add non-terminal edges with the same capacity.
+    #g.add_grid_edges(nodeids, 128)
+    for i in range(img.shape[0]-1) :
+        for j in range(img.shape[1]-1) :
+
+            #gX = alpha*(1-abs(img[i][j]-img[i+1][j]))**25
+            #gY = alpha*(1-abs(img[i][j]-img[i][j+1]))**25
+
+            gX = alpha*delta_func(img[i][j],img[i+1][j])*wij(img[i][j],img[i][j+1])
+            gY = alpha*delta_func(img[i][j],img[i][j+1])*wij(img[i][j],img[i][j+1])
+
+            g.add_edge(nodeids[i][j],nodeids[i+1][j],gX,0)
+            g.add_edge(nodeids[i][j],nodeids[i][j+1],gY,0)
+
+    g.add_grid_tedges(nodeids, R_func(img,0), R_func(img,0.8))
+
+    # Find the maximum flow.
+    g.maxflow()
+    # Get the segments of the nodes in the grid.
+    sgm = g.get_grid_segments(nodeids)
+
+    # The labels should be 1 where sgm is False and 0 otherwise.
+    result = np.int_(np.logical_not(sgm))
+
+    ppl.imshow(result)
+    ppl.show()
 
 def paint_draw(event,former_x,former_y,flags,param):
     global current_former_x,current_former_y,drawing, mode
@@ -63,6 +122,6 @@ if __name__ == "__main__":
             color=(255,0,0)
 
         if k==99: #C Key
-            #Compute
+            computeSegmentation(args.img,image)
 
     cv2.destroyAllWindows()
